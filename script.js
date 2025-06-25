01ae46009fab22583f7266b242ee6216b8675e8b
Bookoostrikes <bookoostrikes77@gmail.com>
2025-06-25 18:14:11 -0400

    Functionality
    -Added an option to rename your pet
    -hunger, mood and boredom now decrease on an interval and display properly

--- script.js
+++ script.js
@@ -4,54 +4,87 @@ let mimikyu = new Mimikyu('Mimikyu', 100, 100, 100);
 
 let feedButton = document.getElementById('feed');
 let playButton = document.getElementById('play');
+let renameButton = document.getElementById('rename');
 
-setInterval(hunger, 10000);
-setInterval(updateHunger, 100);
+setInterval(updateHunger, 1000);
+setInterval(displayHunger, 100);
 setInterval(updateBoredom, 1000);
+setInterval(displayMood, 100);
 
 let petHungerBox = document.getElementById('mimikyuHunger');
 let petHungerText = document.createElement('p');
+petHungerBox.append(petHungerText);
+
 
 let petBoredomBox = document.getElementById('mimikyuBoredom');
 let petBoredomText = document.createElement('p');
+petBoredomBox.append(petBoredomText);
 
-function updateHunger(){
+
+let petMoodBox = document.getElementById('mimikyuMood');
+let petMoodText = document.createElement('p');
+petMoodBox.append(petMoodText);
+
+let actionBox = document.getElementById('actionBox');
+let actionText = document.createElement('p');
+actionBox.append(actionText);
+
+let renameBox = document.getElementById('renameBox');
+let renameText = document.createElement('p');
+
+function displayHunger(){
 if (mimikyu.hunger == 0){  
 petHungerText.textContent = `${mimikyu.name}'s stomach is empty!`;
 }
-else if (mimikyu.hunger < 10){
+else if (mimikyu.hunger <= 10){
 petHungerText.textContent = `${mimikyu.name} is feeling famished...`;
 }
-else if (mimikyu.hunger < 25){
+else if (mimikyu.hunger <= 25){
 petHungerText.textContent = `${mimikyu.name} is starving.`;
 }
-else if (mimikyu.hunger < 50){
+else if (mimikyu.hunger <= 50){
 petHungerText.textContent = `${mimikyu.name} is feeling hungry...`;
 }
 else if (mimikyu.hunger == 100){
 petHungerText.textContent = `${mimikyu.name} is full`;
 }
-else if (mimikyu.hunger > 75 || mimikyu.hunger < 75){
+else if (mimikyu.hunger >= 75 || mimikyu.hunger <= 75){
 petHungerText.textContent = `${mimikyu.name}'s stomach is content`;
 }
-petHungerBox.append(petHungerText);
 }
 
 
-function hunger(){
+function updateHunger(){
+
     if(mimikyu.hunger > 0){
-    mimikyu.hunger -= 10;
+    mimikyu.hunger -= 5;
+
     }
-}
+    if (mimikyu.hunger <= 50){
+        updateMood();
+    }
+} //end of update hunger
 
 feedButton.onclick = () =>{
+if(mimikyu.hunger >= 100){
+mimikyu.hunger = 100;
+actionText.textContent = `${mimikyu.name} is already full!`;
+setTimeout(function(){actionText.textContent = '';}, 3000);
+return;
+}
+
 if(mimikyu.hunger < 100){
-mimikyu.hunger += 5;
+mimikyu.hunger += 100;
+actionText.textContent = `${mimikyu.name} is happily eating the food!`;
+setTimeout(function(){actionText.textContent = '';}, 3000);
+    }
+
+
+if(mimikyu.hunger >= 100){
+mimikyu.hunger = 100;
 }
-if(mimikyu.hunger > 100){
-mimikyu.hunger == 100;
-}
-}
+
+}// end of feed onclick
 
 function updateBoredom(){
     mimikyu.boredom -= 1;
@@ -71,11 +104,55 @@ petBoredomText.textContent = `${mimikyu.name} is enjoying itself.`;
 else if (mimikyu.boredom <= 100 || mimikyu.boredom >= 75){  
 petBoredomText.textContent = `${mimikyu.name} is having so much fun!`;
 }
-petBoredomBox.append(petBoredomText);
 }
 
 playButton.onclick = () =>{
+}
+
+function displayMood(){
+    if (mimikyu.mood <= 0){
+petMoodText.textContent = `${mimikyu.name} hates you...`;
+}
+else if (mimikyu.mood <= 25){
+petMoodText.textContent = `${mimikyu.name} is in a foul mood`;
+}
+else if (mimikyu.mood <= 50){
+petMoodText.textContent = `${mimikyu.name} enjoys your prescene.`;
+}
+else if (mimikyu.mood <= 100 || mimikyu.mood >= 75){  
+petMoodText.textContent = `${mimikyu.name} loves you!`;
+}
+}
+
+function updateMood(){
+    mimikyu.mood = mimikyu.mood + ( (mimikyu.hunger*1.5) + -75);
+    if (mimikyu.mood <= 0){
+        mimikyu.mood = 0;
+    }
+}
+
+renameButton.onclick = () =>{
+    let submitButton = document.createElement('button');
+    submitButton.textContent = 'SUBMIT';
+
+    let submitForm = document.createElement('input');
+
+    renameBox.append(renameText);
+    renameText.textContent = `What would you like to rename ${mimikyu.name} to?`;
+
+    renameBox.append(submitButton);
+    renameBox.append(submitForm);
+
+    submitButton.onclick = () =>{
+    let input = submitForm.value;
+    renameText.textContent = `Congratulations! Your Mimikyu is now named ${input}!`;
+    mimikyu.name = input;
+   renameBox.removeChild(submitButton);
+   renameBox.removeChild(submitForm);
+
+   setTimeout(function(){renameBox.removeChild(renameText);}, 3000);
+    }
+
 
 
 }
-
